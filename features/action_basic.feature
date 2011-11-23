f3685ebf82e7a276cdf39804253e20997a4870f3
Robert Nubel <rnubel@enovafinancial.com>
2011-11-22 23:05:48 -0600

    Cucumber!

---
+++ features/action_basic.feature
@@ -0,0 +1,10 @@
+Feature: Actions
+  In order to model the effect of an entity performing an action
+  I want to be able to be able to apply the action to an entity
+  
+  Scenario:
+  Given I know of one entity
+  And the entity performs the action Murder
+  Then my feelings toward the entity should be ???
+
+
---
+++ features/step_definitions/action_steps.rb
@@ -0,0 +1,12 @@
+Given /I know of one entity/ do 
+  entity = Entity.new
+end
+
+Given /^the entity performs the action Murder$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^my feelings toward the entity should be \?\?\?$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
