bcb21c04634b6df537ba2435a21780b8fc82f874
Robert Nubel <rnubel@enovafinancial.com>
2011-11-27 01:51:32 -0600

    Adds test to verify Action::Murder instantiates with a context. Need to pull this up to Action after I think about how behavior should be extracted across all Actions...

--- spec/action_murder_spec.rb
+++ spec/action_murder_spec.rb
@@ -1,8 +1,10 @@
 require File.expand_path('spec/spec_helper')
 
 describe Actions::Murder do
-  it "should instantiate with a context" do
-    fail
+  it "should instantiate with a context as a hash" do
+    action = Actions::Murder.new({})
+
+    action.should_not be_nil
   end
 
 end
