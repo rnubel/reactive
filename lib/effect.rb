2318cf0a9ab049d259e4febe3432780252996aef
Robert Nubel <rnubel@enovafinancial.com>
2011-11-30 15:22:06 -0600

    Adds stub of update_emotions to Effect.

--- lib/effect.rb
+++ lib/effect.rb
@@ -1,5 +1,7 @@
 require File.expand_path('bootstrap')
 
 class Effect
+  def update_emotions(emotions)
 
+  end
 end
--- spec/effect_spec.rb
+++ spec/effect_spec.rb
@@ -4,4 +4,10 @@ describe Effect do
   it "should instantiate" do
     Effect.new.should_not be_nil
   end
+
+  it "should respond to update_emotions and take one argument" do
+    e = Effect.new
+
+    e.update_emotions(nil)
+  end
 end
