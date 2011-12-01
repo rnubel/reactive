49e4ddee408100c8d02e6712379fb9d3fde83f62
Robert Nubel <robert.nubel@gmail.com>
2011-11-30 23:53:24 -0600

    Implements and tests a basic algorithm for computing feelings-change for Actions::Murder.
    Emotions-change is still unspecified.

--- lib/action_murder.rb
+++ lib/action_murder.rb
@@ -1,11 +1,26 @@
 module Actions
   class Murder
+    attr_accessor :source_entity, :destination_entity
     def initialize(context)
-
+      @source_entity = context[:source]
+      @destination_entity = context[:destination]
     end
 
     def compute_effect
-      Effect.new
+      sentiment_src = @source_entity.sentiment_towards
+      sentiment_dst = @destination_entity.sentiment_towards
+
+      # TODO: pull up
+      feelings_change_base = {
+                                :like => -1,
+                                :fear => 1
+                             }
+      feelings_change = {}
+      feelings_change_base.each do |attr, val|
+        feelings_change[attr] = val * sentiment_dst.score
+      end
+
+      Effect.new({}, { @source_entity => feelings_change })
     end
   end
 end
--- spec/action_murder_spec.rb
+++ spec/action_murder_spec.rb
@@ -1,24 +1,42 @@
 require File.expand_path('spec/spec_helper')
 
 describe Actions::Murder do
+  before :each do
+    # Mock out objects
+    @s = stub(:score => 1)
+    @e_src = stub(:sentiment_towards => @s)
+    @e_dst = stub(:sentiment_towards => @s)
+  end
+
   it "should instantiate with a context as a hash" do
     action = Actions::Murder.new({})
 
     action.should_not be_nil
   end
 
+  it "should pull out source and dest entities from context" do
+    action = Actions::Murder.new({:source => @e_src, :destination => @e_dst})
+
+    action.source_entity.should == @e_src
+    action.destination_entity.should == @e_dst
+  end
+
   it "should compute its effect as an Effect" do
-    action = Actions::Murder.new({})
+    action = Actions::Murder.new({:source => @e_src, :destination => @e_dst})
 
     action.compute_effect.should be_an(Effect)
   end
 
   it "should compute the correct effect given a specific context" do
-    e_src = mock()
-    e_dst = mock()
-    action = Actions::Murder.new({:source => e_src, :destination => e_dst})
 
-    expected_effect = :totally_the_right_effect
+    action = Actions::Murder.new({:source => @e_src, :destination => @e_dst})
+
+    expected_effect = Effect.new(
+      {}, # Not sure yet
+      {
+        @e_src => { :like => -1, :fear => 1 }
+      }
+    )
 
     action.compute_effect.should == expected_effect
   end
