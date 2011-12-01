7789d8d92b6bc2bbcfc04849151bdf0cec314423
Robert Nubel <robert.nubel@gmail.com>
2011-11-30 23:26:03 -0600

    Adds meat to the Effect class's internals.
    Specifically, adds the emotions_change and feelings_change properties. Emotions change should describe how the Emotions of Self change upon reacting, and feelings change should describe how feelings toward each entity should change.

--- lib/effect.rb
+++ lib/effect.rb
@@ -1,7 +1,22 @@
 require File.expand_path('bootstrap')
 
 class Effect
-  def update_emotions(emotions)
+  # Only for testing/comparison
+  attr_reader :emotions_change, :feelings_change
 
+  def initialize(emotions_change = {}, feelings_change = {})
+    @emotions_change = emotions_change
+    @feelings_change = feelings_change
+  end
+
+  def update_emotions(emotions)
+    self.emotions_change.each do |attribute, value|
+      emotions.change_attribute_by(attribute, value)
+    end
+  end
+
+  def ==(other)
+    self.emotions_change == other.emotions_change &&
+    self.feelings_change == other.feelings_change
   end
 end
--- spec/effect_spec.rb
+++ spec/effect_spec.rb
@@ -10,4 +10,25 @@ describe Effect do
 
     e.update_emotions(nil)
   end
+
+  it "should instantiate with an emotions delta and feelings delta" do
+    ent = Entity.new
+    e = Effect.new({:happiness => -1}, { ent => {:like => -1} })
+
+    e.should_not be_nil
+  end
+
+  it "should update emotions per its emotions_change" do
+    e = Effect.new({:blah => 1}, {})
+
+    em = mock()
+    em.expects(:change_attribute_by).with(:blah, 1).returns(nil)
+
+    e.update_emotions(em)
+  end
+
+  it "should compare via the equality operator to other Effects" do
+    Effect.new( {:blah => 1}, {:arg => 0} ).should == 
+       Effect.new( {:blah => 1}, {:arg => 0} ) 
+  end
 end
