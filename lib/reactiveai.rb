d99748d14657715ae63440a13057abc054f48ce5
rnubel <robert.nubel@gmail.com>
2011-12-11 00:10:33 -0600

    Replaces magic require with explicit requires.

--- lib/reactiveai.rb
+++ lib/reactiveai.rb
@@ -1,3 +1,8 @@
-Dir["reactiveai/*.rb"].each do |file|
-  require file
-end
+require 'reactiveai/self'
+require 'reactiveai/emotions'
+require 'reactiveai/personality'
+require 'reactiveai/entity'
+require 'reactiveai/sentiment'
+require 'reactiveai/action'
+require 'reactiveai/action_murder'
+require 'reactiveai/feelings'
